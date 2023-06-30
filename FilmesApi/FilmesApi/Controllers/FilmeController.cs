60a743328859c92c871a869b6c6dbaddcc944e67
Andre Henriques <andrehenriquesb@gmail.com>
2023-06-29 23:15:52 -0300

    api de filmes

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class FilmeController: ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        if (!string.IsNullOrEmpty(filme.Titulo))
+        {
+            Console.WriteLine(filme.Duracao);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Genero);
+            filmes.Add(filme);
+        }
+    }
+}
+
