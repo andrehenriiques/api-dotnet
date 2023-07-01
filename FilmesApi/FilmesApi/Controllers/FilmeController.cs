7010bac418cd07035186d713817087b24bb38a04
Andre Henriques <andrehenriquesb@gmail.com>
2023-06-30 21:28:19 -0300

    post, get, getid, paginacao

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -10,17 +10,28 @@ public class FilmeController: ControllerBase
 {
 
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    { 
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperaFilmeId), new {id = filme.Id}, filme); 
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilme([FromQuery]int skip = 0, [FromQuery] int take = 5)
     {
-        if (!string.IsNullOrEmpty(filme.Titulo))
-        {
-            Console.WriteLine(filme.Duracao);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Genero);
-            filmes.Add(filme);
-        }
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmeId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 }
 
