a97d24df015f069b8eed9b6857a4846b8185a65b
Andre Henriques <andrehenriquesb@gmail.com>
2023-07-13 00:27:09 -0300

    relacao n:n, consulta

--- FilmesApi/FilmesApi/Data/Dtos/ReadSessaoDto.cs
+++ FilmesApi/FilmesApi/Data/Dtos/ReadSessaoDto.cs
@@ -3,7 +3,9 @@ namespace FilmesApi.Data.Dtos;
 
 public class ReadSessaoDto
 {
-    public int Id { get; set; }
+    public int FilmeId { get; set; }
+
+    public int CinemaId { get; set; }
 
     //public ReadFilmeDto Filme { get; set; }
 }
